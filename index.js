8260f7b1f3b7be8715afe7d5655c774f2a91d7ad
abuabdullah23 <abuobaidazayed@gmail.com>
2023-11-08 23:43:46 +0600

    Added some site url for cors policy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://trip-thrive-client.vercel.app', 'https://trip-thrive-8e2c6.web.app'],
     credentials: true,
 }));
 app.use(express.json());
